809de4ae416019a37df0315c238c3ed9b0f2cc3d
sharmila Prabu <132900368+Sharmilaprabu@users.noreply.github.com>
2024-09-04 19:58:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
+const express = require('express')
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const app = express()
 
-const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('MongoDB connected...');
+app.use(express.json());
 
-    }catch(err) {
-        console.error('MangoDB connection error:', err);
-        throw err;
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server Updated")
+});
+
+app.get('/api/products', async(req, res) =>{
+
+    try {
+        res.status(200) = await Product.find({});
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-}
 
-module.exports = connectDB;
+});
+app.post('/api/products', async (req, res) => {
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    }catch(error) {
+        res.status(500).json({message: error.message});
+
+    }
+});
+
+
+mongoose.connect("mongodb+srv://sharmilaprabu05:qI2LIKmL13VOPrFc@backenddb.e9qye.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(() =>{
+console.log('Connected to database!');
+})
+.catch(() => {
+    console.log("connection failed");
+});
